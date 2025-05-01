b43351086569c0b96886abf2492b7b43550c8c85
dehuihu <dehuihu@gmail.com>
2025-05-01 10:55:42 -0700

    Refactor cat_routes by adding to_dict and from_dict, validate_model

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -2,30 +2,19 @@ from flask import Blueprint, abort, make_response, request, Response
 from sqlalchemy import or_
 from ..db import db
 from app.models.cat import Cat
-
+from .route_utilities import validate_model
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix = "/cats")
 
 @cats_bp.post("")
 def create_cat():
     request_body = request.get_json()
-    name = request_body["name"]
-    color = request_body["color"]
-    personality = request_body["personality"]
-
-    new_cat = Cat(name=name, color=color, personality=personality)
+    new_cat = Cat.from_dict(request_body)
 
     db.session.add(new_cat)
     db.session.commit()
 
-    response = {
-        "id": new_cat.id,
-        "name": new_cat.name,
-        "color": new_cat.color,
-        "personality": new_cat.personality
-    }
-
-    return response, 201
+    return new_cat.to_dict(), 201
 
 @cats_bp.get("")
 def get_all_cats():
@@ -51,47 +40,20 @@ def get_all_cats():
 
     for cat in cats:
         cats_response.append(
-            {
-                "id": cat.id,
-                "name": cat.name,
-                "color": cat.color,
-                "personality": cat.personality
-            }
+            cat.to_dict()
         )
     return cats_response
 
 @cats_bp.get("/<id>")
 def get_one_cat(id):
-    cat = validate_cat(id)
+    cat = validate_model(Cat, id)
 
-    return {
-            "id": cat.id,
-            "name": cat.name,
-            "color": cat.color,
-            "personality": cat.personality
-        }
+    return cat.to_dict()
 
-def validate_cat(id):
-    try:
-        id = int(id)
-    except:
-        invalid = {"message": f"Cat id ({id}) is invalid."}
-        abort(make_response(invalid, 400))
 
-    query = db.select(Cat).where(Cat.id == id)
-    cat = db.session.scalar(query)
-
-    if not cat:
-        response = {"message": f"cat {id} not found."}
-        abort(make_response(response, 404))
-
-    return cat   
-    # for cat in cats:
-    #     if cat.id == id:
-    #         return cat
 @cats_bp.put("/<id>")
 def update_cat(id):
-    cat = validate_cat(id)
+    cat = validate_model(Cat, id)
 
     request_body = request.get_json() 
 
@@ -105,7 +67,7 @@ def update_cat(id):
 
 @cats_bp.delete("/<id>")
 def delete_cat(id):
-    cat = validate_cat(id)
+    cat = validate_model(Cat, id)
 
     db.session.delete(cat)
 
@@ -126,18 +88,11 @@ def delete_cat(id):
 #             personality = cat.personality
 #         ))
 #     return results_list
-          
+
+
+
         
         
 
-# @cats_bp.get("/<id>")
-# def get_one_cat(id):
-#     cat = validate_cat(id)
-#     cat_dict = dict(
-#         id = cat.id,
-#         name = cat.name,
-#         color = cat.color,
-#         personality = cat.personality
-#     )
-#     return cat_dict
+
 
